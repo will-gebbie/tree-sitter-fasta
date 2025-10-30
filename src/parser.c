53915d1455deea63611584661be4b7015cde6709
Will <will.gebbie777@gmail.com>
2025-10-30 16:19:22 -0700

    Release 1.0.6

--- src/parser.c
+++ src/parser.c
@@ -532,7 +532,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_fasta(void) {
     .metadata = {
       .major_version = 1,
       .minor_version = 0,
-      .patch_version = 5,
+      .patch_version = 6,
     },
   };
   return &language;

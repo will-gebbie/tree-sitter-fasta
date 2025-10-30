7b5789d9fdf362276e88dd67e7c399d029a08bcf
Will <will.gebbie777@gmail.com>
2025-10-30 16:15:21 -0700

    generate parser.c

--- src/parser.c
+++ src/parser.c
@@ -532,7 +532,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_fasta(void) {
     .metadata = {
       .major_version = 1,
       .minor_version = 0,
-      .patch_version = 1,
+      .patch_version = 5,
     },
   };
   return &language;
